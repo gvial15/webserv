c4ecf0f1a6644f95e1ac550dc8da0e95c91568e2
me <guillaumevial15@gmail.com>
2023-03-30 16:06:28 -0400

    nginx config: modified file srcs

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "main.hpp"
+
+int	main(int argc, char **argv)
+{
+	// parse config file
+	// create server(s) from config
+	// start listening
+
+	// event loop:
+	//  - receive request
+	//  - formulate response
+	//  - send response
+	//  - start again...
+}
---
+++ main.hpp
@@ -0,0 +1,12 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+// read & write
+# include <unistd.h>
+# include <fcntl.h>
+
+// socket stuff
+# include <sys/socket.h>
+# include <arpa/inet.h>
+
+#endif
