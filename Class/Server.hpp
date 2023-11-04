c534bc9305996f35b7a59896e9e96136aa038a08
gvial15 <guillaumevial15@gmail.com>
2023-11-04 16:34:37 -0400

    Server::root , Server::index, Server::autoindex, Server::client_max_body_size, Server::error_pages setters added

--- Class/Server.hpp
+++ Class/Server.hpp
@@ -23,6 +23,16 @@ class Server {
         std::size_t							get_client_max_body_size() const { return client_max_body_size; }
         std::map<std::string, std::string>	get_error_pages() const { return error_pages; }
 
+        // setters
+        void set_server_fd(int fd) { server_fd = fd; }
+        void set_port(int p) { port = p; }
+        void set_ip(const std::string &new_ip) { ip = new_ip; }
+        void set_root(const std::string &new_root) { root = new_root; }
+        void set_index(const std::string &new_index) { index = new_index; }
+        void set_autoindex(std::size_t new_autoindex) { autoindex = new_autoindex; }
+        void set_client_max_body_size(std::size_t size) { client_max_body_size = size; }
+        void set_error_pages(const std::map<std::string, std::string> &pages) { error_pages = pages; }
+
         // public attributes
 
     private:
