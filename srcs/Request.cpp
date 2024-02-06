cfb34ffbfadc395e9e90baa9d725c017667626b6
Asael Rivera <arivera@c1r7p7.42quebec.com>
2024-02-06 14:08:14 -0500

    clean up

--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -6,20 +6,20 @@
 /*   By: arivera <arivera@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 12:43:17 by arivera           #+#    #+#             */
-/*   Updated: 2024/02/06 13:59:03 by arivera          ###   ########.fr       */
+/*   Updated: 2024/02/06 14:07:43 by arivera          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Class/Request.hpp"
 
+Request::Request( const char *request ): _request(request){
+    this->parse();
+}
+
 void    Request::printRequestElems() const{
     for (std::map<std::string, std::string>::const_iterator it = this->_requestElem.begin(); it != _requestElem.end(); ++it) {
         std::cout << it->first << " -> " << it->second << std::endl;
     }
-}
-
-Request::Request( const char *request ): _request(request){
-    this->parse();
 }
 
 void Request::stringToLower(std::string &str) {
